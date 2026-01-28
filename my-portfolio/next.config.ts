a24cef04c44d0b344af877de5b7af43873fd552c
deepak-karkala <dkarkala01@gmail.com>
2026-01-28 13:56:06 +0530

    Package json files

--- my-portfolio/next.config.ts
+++ my-portfolio/next.config.ts
@@ -2,6 +2,20 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/case-studies',
+        destination: '/experiences',
+        permanent: true,
+      },
+      {
+        source: '/case-studies/:slug',
+        destination: '/experiences/:slug',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
